27280527060ee548d4c68360147d9d73a216cb84
XinJack <1055385195@qq.com>
2017-08-16 14:17:26 +0800

    add some guide codes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,15 @@
 $(function(){
-	$('#realtimeWatch').on('click', function(e){
-		e.preventDefault()
-		console.log('watch')
-	})
+	// 为a标签添加点击事件
+	$('#realtimeWatch').on('click', realtimeWatchImplement);
+
+	// 编写回调函数
+	function realtimeWatchImplement(event){
+		// 阻止默认的点击事件处理
+		event.preventDefault()
+
+		// 编写自己的功能实现代码
+		// ... 
+		// ...
+		console.log('realtimeWatch')
+	}
 })
